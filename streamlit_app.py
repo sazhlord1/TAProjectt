3a97baeb64f654bdf6a0a69d0283c3743dac007d
sazhlord1 <109146915+sazhlord1@users.noreply.github.com>
2025-12-31 20:59:40 +0330

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -448,8 +448,8 @@ def page_define_techs():
         """)
     
     # Number of technologies selector
-    st.subheader("Number of Technologies")
-    num_tech = st.slider(
+st.subheader("Number of Technologies")
+num_tech = st.slider(
     "How many technologies do you want to assess?",
     min_value=1,
     max_value=min(MAX_TECH, len(tech_df)),
@@ -457,10 +457,10 @@ def page_define_techs():
     help="You can assess between 1 and 20 technologies"
 )
 
-    )
-    st.session_state['num_tech'] = num_tech
-    
-    st.divider()
+st.session_state['num_tech'] = num_tech
+
+st.divider()
+
     
     # Technology definition inputs
     st.subheader("Define Each Technology")
